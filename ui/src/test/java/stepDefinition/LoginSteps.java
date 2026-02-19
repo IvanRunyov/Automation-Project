7bb8d338efe666fe9b7cac03d7449196a320dd4f
yoan.zhelyazkov <yoan.zhelyazkov@egt-digital.com>
2026-02-19 17:43:14 +0200

    Removed obsolete PendingException import.

--- ui/src/test/java/stepDefinition/LoginSteps.java
+++ ui/src/test/java/stepDefinition/LoginSteps.java
@@ -1,7 +1,6 @@
 package stepDefinition;
 
 import Drivers.PlaywrightPageProvider;
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
